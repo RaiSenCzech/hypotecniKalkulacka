1c2067a8aeb97a1f94b0f48fa10c6417f1edec99
RaiSenCzech <50031296+RaiSenCzech@users.noreply.github.com>
2020-10-20 13:14:14 +0200

    Create MainPage.xaml.cs

---
+++ MainPage.xaml.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HypotecniKalkulacka
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            BindingContext = new VypocetHypoteky();
+
+            InitializeComponent();
+        }
+    }
+}
